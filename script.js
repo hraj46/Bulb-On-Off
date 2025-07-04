99deac66279c14b84020b103a89b04766a0018ed
Himanshu Raj <rajhimanshu7452@gmail.com>
2025-07-04 11:09:04 +0530

    Minor Changes

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ let flag = false;
 button.addEventListener("click", ()=> {
     if(flag === false){
         bulb.src = "https://www.w3schools.com/js/pic_bulbon.gif";
-        button.innerHTML = "Turn off";
+        button.innerHTML = "Turn Off";
         flag = true;
     } else {
         bulb.src = "https://www.w3schools.com/js/pic_bulboff.gif";
-        button.innerHTML = "Turn on";
+        button.innerHTML = "Turn On";
         flag = false;
     }
 })
